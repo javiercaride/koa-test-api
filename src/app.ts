149fa43fc4fbc165ca76ce22570f2587a9cbfa8f
Javier Caride Ulloa <javier.caride@gmail.com>
2021-06-19 17:59:08 +0200

    Fixing lint code

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
-import Koa from "koa"
-import Router from "koa-router";
+import Koa from 'koa';
+import Router from 'koa-router';
+import BodyParser from 'koa-bodyparser';
 
-const application = new Koa();  
+const application = new Koa();
 const router = new Router();
 
-router.get("/", async (ctx,next) => {
-  ctx.body = "Hello Koa!";
+router.get('/', async (ctx, next) => {
+  ctx.body = { hello: 'Koa' };
 
   await next();
-})
+});
 
 application.use(router.routes()).use(router.allowedMethods());
+application.use(BodyParser());
 
 application.listen(3000);
